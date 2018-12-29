23ea983ac29e94dd7bceef2f4d1a8d2f46f0e83f
Thomas Lindauer <thomaslindauer@mail.com>
2018-12-29 17:11:40 +0100

    remove recursive

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rand::Rng;
 
 fn generate_random() -> u32 {
 
+    println!("Guess the random number!"); 
     rand::thread_rng().gen_range(1, 101)
 
 }
@@ -11,50 +12,78 @@ fn generate_random() -> u32 {
 fn input_guess() -> u32 {
 
     let mut guess = String::new();
-
+    
     io::stdin().read_line(&mut guess)
     .expect("Failed to read line");
 
     let guess: u32 = match guess.trim().parse() {
+
         Ok(num) => num,
-        Err(_) => input_guess(),
+        Err(_) => {
+            println!("Only numbers please");
+            input_guess()
+        }
+
     };
 
     guess
 
 }
 
-fn evaluate_guess(guess: &u32, secret_number: &u32) {
+fn evaluate_guess(guess: &u32, secret_number: &u32) -> bool {
 
     match guess.cmp(&secret_number) {
 
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
+        Ordering::Less => {
+            println!("Too small!");
+            false
+        },
+        Ordering::Greater => { 
+            println!("Too big!");
+            false
+        },
         Ordering::Equal => {
             println!("You win!");
-            main();
+            true
         }
 
-    };
+    }
+
+}
+
+fn guess_session(secret_number: &u32) -> bool {
+
+    let mut won_guess = false;
+
+    while !won_guess {
+
+        println!("Please input your guess");
+        let guess = input_guess();
+
+        println!("You guessed: {}", guess);
+        won_guess = evaluate_guess(&guess, &secret_number)
+
+    }
+
+    won_guess
+
+}
+
+fn stage_guess() {
+
+    let mut secret_number = generate_random();
+
+    if guess_session(&secret_number) {
+
+        secret_number = generate_random();
+        guess_session(&secret_number);
+
+    }
 
 }
 
 fn main() {
 
-    println!("Guess the number!");
-
-    let secret_number = generate_random();
-
-    loop {
-        
-        println!("Please input your guess.");
-
-        let guess = input_guess();
-
-        println!("You guessed: {}", guess);
-
-        evaluate_guess(&guess, &secret_number);
-
-    }
+    stage_guess()
 
 }
